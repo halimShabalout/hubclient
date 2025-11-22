6718d82bf43667dc3c31a0567a40baa92e95e092
halimShabalout <halim.codes@gmail.com>
2025-11-23 02:36:34 +0300

    feat: AboutUs section & page with translations

---
+++ lib/hooks/useAboutUs.ts
@@ -0,0 +1,11 @@
+import { useQuery } from "@tanstack/react-query";
+import { AboutUs } from "../types/AboutUs";
+import { ApiResponse } from "../types/ApiResponse";
+import { fetchAboutUs } from "../services/aboutUsService";
+
+export const useAboutUs = (language: string) => {
+  return useQuery<ApiResponse<AboutUs[]>>({
+    queryKey: ["aboutUs", language],
+    queryFn: () => fetchAboutUs(language),
+  });
+};
